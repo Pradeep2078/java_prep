6bc8eed39c086130cd23f3def099ce55d36f68ee
Pradeepkumar <pradeep.pal@vishleshan.net>
2026-09-23 00:44:48 +0530

    first question odd and even paste

---
+++ basic_dsa_warmup/number_and _math_questions/FirstQuestionOddEven.java
@@ -0,0 +1,11 @@
+class FirstQuestionOddEven {
+    public static void main(String args[]){
+        // Check whether the number odd or even
+        int num = 1;
+        String checkValue = num % 2 == 0 ? "It is Even....": "It is Odd....";
+        
+        System.out.println(checkValue);
+      }
+}
+
+// Input should be the 0, 1, 2, 3, ....
